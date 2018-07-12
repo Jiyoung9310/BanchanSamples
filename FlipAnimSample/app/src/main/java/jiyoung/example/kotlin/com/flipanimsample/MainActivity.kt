afcae0a8ea6d7865a3959ff6a23c857511ca56bb
jiyoung <jungee9732@osstem.com>
2018-07-12 10:57:08 +0900

    CardFlip Motion Sample - click ( not swipe )

---
+++ FlipAnimSample/app/src/main/java/jiyoung/example/kotlin/com/flipanimsample/MainActivity.kt
@@ -0,0 +1,33 @@
+package jiyoung.example.kotlin.com.flipanimsample
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        main_activity_card_face.setOnClickListener { _ -> onCardClick() }
+        main_activity_card_back.setOnClickListener { _ -> onCardClick() }
+    }
+
+    fun onCardClick() {
+        flipCard()
+    }
+
+    private fun flipCard() {
+
+        val flipAnimation = FlipAnimation(main_activity_card_face, main_activity_card_back)
+
+        if (main_activity_card_face.visibility == View.GONE) {
+            flipAnimation.reverse()
+        }
+        main_activity_root.startAnimation(flipAnimation)
+    }
+}
+
+
